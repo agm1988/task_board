db17b0e84d680a7c33672a926d5cb89161d9c1c3
Alexandr <Gsashko@mail.ru>
2017-06-02 21:45:38 +0300

    added pundit tags

---
+++ app/policies/tag_policy.rb
@@ -0,0 +1,52 @@
+class TagPolicy < ApplicationPolicy
+  def permitted_attributes
+    if user.is_admin?
+      [:name]
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.is_admin?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.is_admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.is_admin?
+  end
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
