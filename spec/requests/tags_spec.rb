57ef151b7a0d14b13ffa89ec4a28e059c5756f0c
Alexandr <Gsashko@mail.ru>
2017-06-06 23:24:29 +0300

    spec clean

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Tags", type: :request do
+RSpec.describe 'Tags', type: :request do
   let!(:admin) { create(:user, :admin) }
 
   before do
     sign_in(admin)
   end
 
-  describe "GET /tags" do
-    it "works! (now write some real specs)" do
+  describe 'GET /tags' do
+    it 'works!' do
       get tags_path
       expect(response).to have_http_status(200)
     end
