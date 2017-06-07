63f8480a6e8ff0af2d5d3d79cc0b6e2ff5a1ff86
Alexandr <Gsashko@mail.ru>
2017-06-07 22:58:12 +0300

    cleanup

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,22 +6,17 @@ class ReportsController < ApplicationController
   after_action :verify_authorized
   after_action :verify_policy_scoped, only: :index
 
-  # GET /reports
-  # GET /reports.json
   def index
     authorize Report
-    @reports = policy_scope(Report).eager_load(:user)
+    @reports = policy_scope(Report).eager_load(:user).page(params[:page])
   end
 
-  # GET /reports/1
-  # GET /reports/1.json
   def show
     authorize @report
     @commentable = @report
     @comment = Comment.new
   end
 
-  # GET /reports/new
   def new
     authorize Report
 
@@ -29,55 +24,37 @@ class ReportsController < ApplicationController
     @report.tasks.build
   end
 
-  # GET /reports/1/edit
   def edit
     authorize @report
   end
 
-  # POST /reports
-  # POST /reports.json
   def create
     authorize Report
 
     @report = Report.new(permitted_attributes(Report))
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: 'Report was successfully created.' }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to @report, notice: 'Report was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /reports/1
-  # PATCH/PUT /reports/1.json
   def update
     authorize @report
 
-    respond_to do |format|
-      if @report.update(permitted_attributes(@report))
-        format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.update(permitted_attributes(@report))
+      redirect_to @report, notice: 'Report was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /reports/1
-  # DELETE /reports/1.json
   def destroy
     authorize @report
 
     @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: 'Report was successfully destroyed.'
   end
 
   def mark_as_reported
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'simplecov'
-require "pundit/rspec"
+require 'pundit/rspec'
 require 'aasm/rspec'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
